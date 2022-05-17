ba1f413903f6d6db3b6bd8dcba5d4489ecd40bcd
pranali2808 <ranepranali1997@gmail.com>
2022-05-17 15:36:36 +0530

    Add employee to payroll without thread

---
+++ EmployeePayrollMultithreading/EmployeePayrollOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollMultithreading
+{
+    public class EmployeePayrollOperation
+    {
+
+        public List<EmployeeDetails> list = new List<EmployeeDetails>();
+        public void AddEmployeeToPayroll(List<EmployeeDetails> list)
+        {
+            list.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                AddEmployeePayroll(employeeData);
+                Console.WriteLine("Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine("No. of employees: " + EmployeeCount());
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine("Thread: " + thread.ManagedThreadId);
+        }
+
+        public void AddEmployeePayroll(EmployeeDetails emp)
+        {
+            list.Add(emp);
+        }
+        public int EmployeeCount()
+        {
+            return this.list.Count;
+        }
+    }
+}
+
--- EmployeePayrollMultithreading/Program.cs
+++ EmployeePayrollMultithreading/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,19 @@ namespace EmployeePayrollMultithreading
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee payroll using multithreading");
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 1, EmployeeName = "Pranali" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 2, EmployeeName = "Jay" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 3, EmployeeName = "Avinash" });
+            employeeDetails.Add(new EmployeeDetails { EmployeeID = 4, EmployeeName = "Nikhil" });
+
+            EmployeePayrollOperation repo = new EmployeePayrollOperation();
+            Stopwatch stopWatch1 = new Stopwatch();
+            stopWatch1.Start();
+            repo.AddEmployeeToPayroll(employeeDetails);
+            stopWatch1.Stop();
+            Console.WriteLine("Duration without multi thread: " + stopWatch1.ElapsedMilliseconds + " ms");
+
             Console.ReadLine();
         }
     }
